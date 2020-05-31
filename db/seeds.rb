fb53b63829097a5c99a1110f51999c3250668db0
github taylorwatsonb <github taylorwatsonb@gmail.com>
2020-05-30 20:14:02 -0400

    food group model and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fruits = FoodGroup.create(name: "Fruits")
+vegetables = FoodGroup.create(name: "Vegetables")
+meats = FoodGroup.create(name: "Meats")
+
+FoodList.create(title: "Apple", description: "Apples are a popular fruit, containing antioxidants, vitamins, dietary fiber, and a range of other nutrients. Due to their varied nutrient content, they may help prevent several health conditions.", image_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.heb.com%2Fis%2Fimage%2FHEBGrocery%2F000320625&imgrefurl=https%3A%2F%2Fwww.heb.com%2Fproduct-detail%2Ffresh-pink-lady-apples%2F320625&tbnid=XvboS3yOkJBboM&vet=12ahUKEwi0vaTC6NzpAhUXPd8KHQK4BG4QMygBegUIARDxAg..i&docid=2YPj74fI3b5hGM&w=2000&h=2000&q=apples&ved=2ahUKEwi0vaTC6NzpAhUXPd8KHQK4BG4QMygBegUIARDxAg", food_group_id: fruit.id)
